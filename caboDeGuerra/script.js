1cc475d9fa1cc0390000faa2a38c76685707b22f
emilynvs <emilynascimento22@outlook.com>
2025-04-09 23:49:29 -0300

    Atualizando arquivos

--- caboDeGuerra/script.js
+++ caboDeGuerra/script.js
@@ -1,36 +1,66 @@
 // criar duas listas com tres jogadores cada
-personagens = ["", "", ""]
-viloes = ["", "", ""]
+
 listaPossiveisViloes = ["Coringa", "Úrsula", "Malévola", "Thanos", "Jafar"]
 
-forcaPersonagem = 0
-forcaViloes = 0
 
-let cont = 0
-for (let i = 0; i < 3; i++) {
-    personagens[i] = prompt("Digite um nome do seu personagem")
-    forcaPersonagem += Math.floor(Math.random() * 10) + 1
-}
+resposta = document.getElementById('resultadoFinal')
 
-while (cont < 3) {
-    escolha = Math.floor(Math.random() * 5)
-    let vilao = listaPossiveisViloes[escolha];
+personagem1 = document.getElementById('persongem1')
+personagem2 = document.getElementById('persongem2')
+personagem3 = document.getElementById('persongem3')
+const personagens = [personagem1, personagem2, personagem3]
 
-    if (viloes.includes(vilao)) {
-        continue
+
+
+function resultado() {
+    const msg = forcas()
+    document.getElementById('decisao').style.display = 'block'
+    document.getElementById('jogo').style.display = 'none'
+
+    if (forcaPersonagem > forcaViloes) {
+        resposta.innerText = "Você venceu\n" + msg
+    } else if (forcaViloes > forcaPersonagem) {
+        resposta.innerText = "Você perdeu\n" + msg
     } else {
-        viloes[cont] = vilao
-        forcaViloes += Math.floor(Math.random() * 10) + 1
-        cont++
+        resposta.innerText = "Empate\n" + msg
     }
 }
-msg = "Sua força: " + forcaPersonagem + "\nForça dos vilões: " + forcaViloes
-if (forcaPersonagem > forcaViloes){
-    alert("Você venceu " + msg)
-} else if(forcaViloes > forcaPersonagem){
-    alert("Você perdeu " + msg)
-} else{
-    alert("Empate " + msg)
+
+function forcas() {
+    forcaPersonagem = 0
+    forcaViloes = 0
+    viloes = ["", "", ""]
+    for (let i = 0; i < 3; i++) {
+        forcaPersonagem += Math.floor(Math.random() * 10) + 1
+    }
+    let cont = 0
+    while (cont < 3) {
+        escolha = Math.floor(Math.random() * 5)
+        let vilao = listaPossiveisViloes[escolha];
+
+        if (viloes.includes(vilao)) {
+            continue
+        } else {
+            viloes[cont] = vilao
+            forcaViloes += Math.floor(Math.random() * 10) + 1
+            cont++
+        }
+    }
+    return msg = "Sua força: " + forcaPersonagem + "\nForça dos vilões: " + forcaViloes
+}
+
+function recomecar() {
+    forcaViloes = 0
+    forcaPersonagem = 0
+
+    personagens.forEach(personagem => {
+        if (personagem) personagem.value = '';
+    });
+    
+    
+    document.getElementById('decisao').style.display = 'none'
+    document.getElementById('jogo').style.display = 'block'
+    document.getElementById('resultadoFinal').innerText = '';
 }
 // calcular a força de cada jogador para as duas listas
 
